dc37231658669609b533e8f539e0d83d1d0d4607
petrusjuca <146126655+petrusjuca@users.noreply.github.com>
2025-01-19 18:58:17 -0300

    Create splitchar.js

---
+++ splitchar.js
@@ -0,0 +1,10 @@
+let userName = getFirstName("Carlos-Almeida-Juanito-Gargalo", "-")
+console.log("Seja bem vindo " + userName)
+
+userName = getFirstName("Felipe Silva Han Solo", " ")
+console.log("Seja bem vindo " + userName)
+
+function getFirstName(name, splitChar){
+	let firstName = name.split(splitChar)[0]
+    return firstName
+}
